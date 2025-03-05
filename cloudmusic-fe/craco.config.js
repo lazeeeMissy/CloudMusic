58dba20a3ee8404fb437c46baae1077de8e0a75c
lazeeeMissy <missylazy1121@gmail.com>
2025-03-06 09:18:16 +1100

    less

--- cloudmusic-fe/craco.config.js
+++ cloudmusic-fe/craco.config.js
@@ -1,9 +1,11 @@
 const path = require('path')
+const CracoLessPlugin = require('craco-less')
 
 // 拼接目录
 const resolve = (dir) => path.resolve(__dirname, dir)
 
 module.exports = {
+  plugins:[{plugin: CracoLessPlugin}],
     webpack: {
       alias: {
         '@': resolve('src'),
